6f884da4e8d567bc8fb3147a81a59acb08f69a46
brykdawid <155831829+brykdawid@users.noreply.github.com>
2024-07-15 19:09:56 +0200

    some changes still does not work

--- src/test/java/Zalangolounge2.java
+++ src/test/java/Zalangolounge2.java
@@ -12,11 +12,18 @@ import java.time.Duration;
 import java.util.List;
 
 /*
+CMD
 cd C:\Program Files (x86)\Google\Chrome\Application
 chrome.exe --remote-debugging-port=9222 --user-data-dir=Y:\ChromeProfile
 
+clothing
 reserved xpath = "//button[@class='styles__ArticleSizeButton-sc-1n1fwgw-0 iiqbEt']"Et
 available xpath = "//button[@class='styles__ArbuttticleSizeButton-sc-1n1fwgw-0 duljHq'] //span[text()='XXS']"
+
+shoes
+available class = styles__ArticleSizeButton-sc-1n1fwgw-0 XKwBF
+reserved class = same as clothing
+
  */
 public class Zalangolounge2 {
 
@@ -26,11 +33,11 @@ public class Zalangolounge2 {
         options.setExperimentalOption("debuggerAddress", "127.0.0.1:9222");
         WebDriver driver = new ChromeDriver(options);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get("https://www.zalando-lounge.pl/campaigns/ZZO2TN9/categories/211691638/articles/L4Z21002F-Q11?catalog-type=campaign&navigationSource=catalog");
+        driver.get("https://www.zalando-lounge.pl/campaigns/ZZO2Y1Q/categories/60457365/articles/PA412O091-Q11?catalog-type=campaign&navigationSource=catalog");
         while (true) {
             try {
                 WebElement sizeContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='styles__ArticleSizeItemList-sc-dt4c4z-2 eaaKLK']")));
-                List<WebElement> sizeButtons = sizeContainer.findElements(By.xpath("//button[contains(@class, 'styles__ArticleSizeButton-sc-1n1fwgw-0')]"));
+                List<WebElement> sizeButtons = sizeContainer.findElements(By.xpath("//button[@role='radio'] //span[text()='40']"));
 
                 boolean sizeAvailable = false;
 
@@ -38,12 +45,13 @@ public class Zalangolounge2 {
                     String sizeText = sizeButton.getText();
                     System.out.printf("Checking size: %s\n", sizeText);
 
-                    if (sizeButton.isEnabled() && sizeText.equals("XXS")) {
-                        System.out.printf("Size XXS is available and enabled. Attempting to click.\n");
+                    if (sizeButton.isEnabled()) {
+                        System.out.printf("Size is available and enabled. Attempting to click.\n");
                         sizeButton.click();
                         sizeAvailable = true;
                         break;
                     }
+                    break;
                 }
 
                 if (sizeAvailable) {
